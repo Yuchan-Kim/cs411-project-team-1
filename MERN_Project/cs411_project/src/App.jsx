f1697f46c3946da7cb160703fbe3b73d52e6d70b
Yuchan-Kim <sksk7799@gmail.com>
2023-04-28 15:52:38 -0400

    UI updates

--- MERN_Project/cs411_project/src/App.jsx
+++ MERN_Project/cs411_project/src/App.jsx
@@ -12,8 +12,7 @@ function App() {
   const [name, setName] = useState('');
   const [imageUrl, setImageUrl] = useState('');
   const [savedVideos, setSavedVideos] = useState([]);
-  const [activeSection, setActiveSection] = useState('trending');
-
+  const [currentSection, setCurrentSection] = useState('trending');
 
 
   useEffect(() => {
@@ -42,6 +41,7 @@ function App() {
         withCredentials: true,
       });
       setSavedVideos(response.data.savedVideos);
+      setCurrentSection('saved'); // Update the current section to 'saved'
     } catch (error) {
       console.error("Error fetching saved videos:", error);
       setError("Unable to fetch saved videos.");
@@ -67,138 +67,151 @@ function App() {
   }
   
   const handleFormSubmit = async (event, action) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    if (action === 'search') {
-      try {
-        const response = await axios.post('http://localhost:3001', { searchTerm });
-        setVideoIds([response.data.videoId]);
-        setError('');
-      } catch (error) {
-        setVideoIds([]);
-        setError('Video not found. Please enter a valid search term.');
-      }
-    } else if (action === 'generate') {
-      try {
-        const response = await axios.post('http://localhost:3001/generate');
-        setVideoIds(response.data.videoIds);
-        setError('');
-      } catch (error) {
-        setVideoIds([]);
-        setError('Unable to generate videos.');
-      }
-    } else if (action === 'random') {
-      try {
-        const response = await axios.post('http://localhost:3001/random');
-        setVideoIds(response.data.videoIds);
-        setError('');
-      } catch (error) {
-        setVideoIds([]);
-        setError('Unable to generate random video.');
-      }
+  setCurrentSection('trending'); // Update the current section to 'trending'
+
+  if (action === 'search') {
+    try {
+      const response = await axios.post('http://localhost:3001', { searchTerm });
+      setVideoIds([response.data.videoId]);
+      setError('');
+    } catch (error) {
+      setVideoIds([]);
+      setError('Video not found. Please enter a valid search term.');
     }
-  };
-  const handleSavedClick = () => {
-    setActiveSection('saved');
-    fetchSavedVideos();
-  };
+  } else if (action === 'generate') {
+    try {
+      const response = await axios.post('http://localhost:3001/generate');
+      setVideoIds(response.data.videoIds);
+      setError('');
+    } catch (error) {
+      setVideoIds([]);
+      setError('Unable to generate videos.');
+    }
+  } else if (action === 'random') {
+    try {
+      const response = await axios.post('http://localhost:3001/random');
+      setVideoIds(response.data.videoIds);
+      setError('');
+    } catch (error) {
+      setVideoIds([]);
+      setError('Unable to generate random video.');
+    }
+  }
+};
+
+  
   return (
     <div className="container">
-      <header>
-        <h1>RandTube</h1>
-        <nav>
-          {!isAuthenticated ? (
-            <>
-              <button onClick={() => (window.location.href = 'http://localhost:3001/auth/google')}>
-                Login
+  <header>
+    <h1>RandTube</h1>
+    <nav>
+      {!isAuthenticated ? (
+        <>
+          <button onClick={() => (window.location.href = 'http://localhost:3001/auth/google')}>
+            Login
+          </button>
+          <button onClick={() => (window.location.href = 'http://localhost:3001/register')}>
+            Register
+          </button>
+        </>
+      ) : (
+        <div className="user-info">
+  <div className="profile-image">
+    <img src={imageUrl} alt="Profile" />
+  </div>
+  <div className="user-details">
+    <h3>{name}</h3>
+    <div className="profile-actions">
+      <button onClick={() => (window.location.href = 'http://localhost:3001/logout')}>
+        Logout
+      </button>
+      <button onClick={fetchSavedVideos}>Saved Videos</button>
+    </div>
+  </div>
+</div>
+
+      )}
+    </nav>
+  </header>
+
+  <div className="form-container">
+    <form onSubmit={(e) => handleFormSubmit(e, 'generate')}>
+      <button type="submit">TOP 10 Trending Videos</button>
+    </form>
+
+    {isAuthenticated && (
+      <form onSubmit={(e) => handleFormSubmit(e, 'search')}>
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search for a YouTube video"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button type="submit">Search</button>
+        </div>
+      </form>
+    )}
+
+    {isAuthenticated && (
+      <form onSubmit={(e) => handleFormSubmit(e, 'random')}>
+        <button type="submit">Random Video</button>
+      </form>
+    )}
+  </div>
+
+  {error && <div className="error-message">{error}</div>}
+  {currentSection === 'trending' && videoIds.length > 0 && (
+    <div className="main-content">
+      {videoIds.map((videoId) => (
+        <div key={videoId} className="card">
+          <iframe
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/${videoId}`}
+            title={`Video ${videoId}`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          {isAuthenticated && (
+            <div className="save-button">
+              <button onClick={() => saveVideo(`https://www.youtube.com/watch?v=${videoId}`)}>
+                Save
               </button>
-              <button onClick={() => (window.location.href = 'http://localhost:3001/register')}>
-                Register
-              </button>
-            </>
-          ) : (
-            <div className="user-info">
-              <div className="profile-image">
-                <img src={imageUrl} alt="Profile" width="100" height="100" />
-              </div>
-              <div className="user-details">
-                <span>Name: {name}</span>
-                <button onClick={() => (window.location.href = 'http://localhost:3001/logout')}>
-                  Logout
-                </button>
-                <button onClick={fetchSavedVideos}>Saved Videos</button>
-              </div>
             </div>
           )}
-        </nav>
-      </header>
-  
-      <div className="container">
-        <form onSubmit={(e) => handleFormSubmit(e, 'generate')}>
-          <button type="submit">TOP 10 Trending Videos</button>
-        </form>
-  
-        {isAuthenticated && (
-          <>
-            <form onSubmit={(e) => handleFormSubmit(e, 'search')}>
-              <input
-                type="text"
-                placeholder="Search for a YouTube video"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-              />
-              <button type="submit">Search</button>
-            </form>
-            <form onSubmit={(e) => handleFormSubmit(e, 'random')}>
-              <button type="submit">Random Video</button>
-            </form>
-          </>
-        )}
-  
-        {error && <div className="error-message">{error}</div>}
-        {videoIds.length > 0 && (
-        <div className="main-content">
-          {videoIds.map((videoId) => (
-            <div key={videoId} className="card">
-              <iframe
-                title={videoId}
-                src={`https://www.youtube.com/embed/${videoId}`}
-                frameBorder="0"
-                allowFullScreen
-              />
-              {isAuthenticated && (
-                <div className="save-button">
-                  <button onClick={() => saveVideo(`https://www.youtube.com/watch?v=${videoId}`)}>
-                    Save
-                  </button>
-                </div>
-              )}
-            </div>
-          ))}
         </div>
-      )}
-  
-        {savedVideos.length > 0 && (
-          <div className="main-content">
-            <h2>Saved Videos</h2>
-            {savedVideos.map((video, index) => {
-              const videoId = new URL(video.url).searchParams.get('v');
-              return (
-                <div key={index} className="card">
-                  <iframe
-                    title={videoId}
-                    src={`https://www.youtube.com/embed/${videoId}`}
-                    frameBorder="0"
-                    allowFullScreen
-                  />
-                  <button onClick={() => deleteVideo(video._id)}>Delete</button>
-                </div>
-              );
-            })}
+      ))}
+    </div>
+  )}
+
+  {currentSection === 'saved' && savedVideos.length > 0 && (
+    <div className="main-content">
+      <h2>Saved Videos</h2>
+      {savedVideos.map((video, index) => {
+        const videoId = new URL(video.url).searchParams.get('v');
+        return (
+          <div key={index} className="card">
+            <iframe
+              width="560"
+              height="315"
+              src={`https://www.youtube.com/embed/${videoId}`}
+              title={`Video ${videoId}`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+            <button onClick={() => deleteVideo(video._id)}>Delete</button>
           </div>
+        );
+      })}
+    </div>
+    
         )}
       </div>
-    </div>
   );
   
 }  
